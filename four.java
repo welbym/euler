5ac07032f0a8f7a0a903e3dbc99d521537d1a663
welbym <welbymoore0816@gmail.com>
2020-01-09 00:01:17 -0600

    problem four solution

---
+++ four.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+public class four{
+  public static void main (String[] args){
+    ArrayList<Integer> pal = new ArrayList<Integer>();
+    for(int i=999; i>900; i--){
+      for(int j=999; j>900; j--){
+        String stringNum = Integer.toString(i*j);
+        if(stringNum.substring(0,1).equals(stringNum.substring(5,6))
+          && stringNum.substring(1,2).equals(stringNum.substring(4,5))
+          && stringNum.substring(2,3).equals(stringNum.substring(3,4))){
+          pal.add(i*j);
+        }
+      }
+    }
+    int largest = 0;
+    for (int p: pal){if(p>largest){largest=p;}}
+    System.out.println(largest);
+  }
+}
